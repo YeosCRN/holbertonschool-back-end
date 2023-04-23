59b1b71424b12d93493af54e30d2d2d926287760
YeosCRN <jeoscrn20@outlook.com>
2023-04-23 17:14:30 -0500

    test_to_rep

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Same as before, return to-do list for a given employee 
+ID in CSV format"""
+import requests
+import sys
+import pandas as pd
+
+if __name__ == "__main__":
+    api_url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(api_url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(
+        api_url + "todos", params={"userId": sys.argv[1]}).json()
+
+    tasks_df = pd.DataFrame(
+        todos, columns=["userId", "id", "title", "completed"])
+
+    user_df = pd.DataFrame(data=[user], columns=[
+                           "id", "name", "username", "email", "address", "phone", "website", "company"])
+
+    merged_df = pd.merge(tasks_df, user_df, on="id", how="outer")[
+        ["id", "username", "completed", "title"]]
+
+    filename = str(user.get("git")) + ".csv"
+    merged_df.to_csv(filename, index=False)
---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Exporting in JSON format"""
+import json
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    API_URL = 'https://jsonplaceholder.typicode.com'
+
+    user_id = argv[1]
+    response = requests.get(
+        f'{API_URL}/users/{user_id}/todos',
+        params={'_expand': 'user'}
+    )
+
+    if response.status_code == 200:
+        data = response.json()
+        dictionary = {user_id: []}
+
+        with open(f'{user_id}.json', 'w',
+                  encoding='utf-8') as file:
+            for task in data:
+                actual_dr = {
+                    'task': task['title'],
+                    'completed': task['completed'],
+                    'username': task['user']['username']
+                }
+                dictionary[user_id].append(actual_dr)
+            json.dump(dictionary, file)
+    else:
+        print(f'Error: {response.status_code}')
